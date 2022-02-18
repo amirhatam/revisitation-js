2575a5a82b5c5c7fc03f25883235a44bb00cc446
Amir <amirhatam136@gmail.com>
2022-02-18 22:14:13 +0100

    jour-2, ex-2

---
+++ jour-2/index.js
@@ -0,0 +1,9 @@
+/* - Créez une variable `basic` de valeur `34`
+- Créez une variable `stringified` qui contient la version string de `basic`
+- Affichez `stringified` dans la console */
+
+const basic = 34
+
+const stringified = basic.toString()
+
+console.log(stringified);
