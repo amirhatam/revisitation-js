2803394ae438f900f1b2cd2d1c415806b33e80c6
Amir <amirhatam136@gmail.com>
2022-02-18 22:19:01 +0100

    jour-2, ex-3

---
+++ jour-2/ex-3.js
@@ -0,0 +1,9 @@
+/*- Créez une variable `num` de valeur `1.5`
+- Créez une variable `rounded` qui contient l'arrondi de `num`
+- Affichez `rounded` dans la console*/
+
+
+const num = 1.5
+const rounded = Math.round(num)
+
+console.log(rounded);
