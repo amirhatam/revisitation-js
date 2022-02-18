ffcd0b4aa0aac6027d64b9697944745634252eed
Amir <amirhatam136@gmail.com>
2022-02-18 22:34:26 +0100

    jour-2, ex-5

---
+++ jour-2/ex-5.js
@@ -0,0 +1,16 @@
+/* - Créez une variable `test` de valeur `143` et 
+une variable `bis` de valeur `219`.
+- Utilisez tous les opérateurs de comparaison avec ces 
+variables et affichez les résultats. */
+
+const test = 143;
+const bis = 219;
+
+console.log("test strictement supérieur bis :", test > bis);
+console.log("test strictement inférieur bis :", test < bis);
+console.log("test supérieur ou égal bis :", test >= bis);
+console.log("test inférieur ou égal bis :", test <= bis);
+console.log("test égal à bis :", test == bis);
+console.log("test strictement égal à bis :", test === bis);
+console.log("test différent de bis :", test != bis);
+console.log("test strictement différent de bis :", test !== bis);
