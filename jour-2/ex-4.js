ad6857f6499fe5b75c27f2b03dc086d7a28b1bd5
Amir <amirhatam136@gmail.com>
2022-02-18 22:24:52 +0100

    jour-2, ex-4

---
+++ jour-2/ex-4.js
@@ -0,0 +1,13 @@
+/* - Créer une variable `test` de valeur `12` et une variable `bis` de valeur `5`
+- Utilisez tous les opérateurs arithmétiques sur ces variables et 
+affichez les résultats */
+
+const test = 12
+const bis = 5
+
+console.log("test + bis : ", test + bis);
+console.log("test - bis : ", test - bis);
+console.log("test *  bis : ", test * bis);
+console.log("test / bis : ", test / bis);
+console.log("test ** bis : ", test ** bis);
+console.log("test % bis : ", test % bis);
