4b3f8c0e27a4a2d65d6f7f7283fb4e03f0f37a9c
Amir <amirhatam136@gmail.com>
2022-02-19 16:45:44 +0100

    jour-2, bonus-2, Second soulution with switch

--- jour-2/bonus-2.js
+++ jour-2/bonus-2.js
@@ -23,3 +23,59 @@ if (spring.includes(month)) {
 } else {
     console.log("Winter");
 }
+
+//Second soulution with switch
+var nextMonth = "January";
+
+switch (nextMonth) {
+
+    case "January":
+        console.log("Winter");
+        break;
+
+    case "February":
+        console.log("Winter");
+        break;
+
+    case "March":
+        console.log("Spring");
+        break;
+
+    case "April":
+        console.log("Spring");
+        break;
+
+    case "May":
+        console.log("Spring");
+        break;
+
+    case "June":
+        console.log("Summer");
+        break;
+
+    case "July":
+        console.log("Summer");
+        break;
+
+    case "August":
+        console.log("Summer");
+        break;
+
+    case "September":
+        console.log("autumn");
+        break;
+
+    case "October":
+        console.log("autumn");
+        break;
+
+    case "November":
+        console.log("autumn");
+        break;
+
+    case "December":
+        console.log("Winter");
+
+    default:
+        console.log("That's not a season...");
+}
