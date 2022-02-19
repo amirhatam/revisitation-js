7c69f935f69c826a50351825b7ae3ae5ded8a0d3
Amir <amirhatam136@gmail.com>
2022-02-19 16:02:10 +0100

    jour-2, ex-6

---
+++ jour-2/ex-6.js
@@ -0,0 +1,15 @@
+/*
+- Créez une variable `limit` de valeur `50` et une variable `score` de valeur `64`
+- Créez une condition : si le score est supérieur ou égal à la limite, 
+affichez "Ok good !", sinon affichez "Oh nooo..."
+- Changez la valeur de score pour changer le résultat
+*/
+
+const limit = 50
+const score = 64
+
+if (score >= limit) {
+    console.log("Ok good !");
+} else {
+    console.log("Oh nooo...");
+}
