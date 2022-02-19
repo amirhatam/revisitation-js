de4059df9affa80d9caca567659398e9142cec25
Amir <amirhatam136@gmail.com>
2022-02-19 16:58:27 +0100

    jour-2, bonus-3

---
+++ jour-2/bonus-3.js
@@ -0,0 +1,9 @@
+/*
+- Créez une variable `roundedNumber` de valeur `3.6`.
+- En utilisant uniquement `Math.floor()`, `Math.ceil()`, des opérations 
+mathématiques et un `if () {} else {}` reproduisez le comportement de `Math.round()`
+-Changez la valeur de `roundedNumber` pour tester l'algorithme que vous avez créé avec
+les valuers `3.3`, `3.8` et `12.4` et vérifier que ça marche bien dans tous les cas.
+- Une fois fini, rajoutez un autre algorithme pour obtenir le même resultat 
+mais en convertissant `roundedNumber` en string et en utilissant `substring`.
+*/
