e0897b4a9e9e428ee906bc6e48da091280701307
Amir <amirhatam136@gmail.com>
2022-02-19 18:05:08 +0100

    jour 2, bonus 3, find result in two soulution

--- jour-2/bonus-3.js
+++ jour-2/bonus-3.js
@@ -8,4 +8,35 @@ les valuers `3.3`, `3.8` et `12.4` et vérifier que ça marche bien dans tous le
 mais en convertissant `roundedNumber` en string et en utilissant `substring`.
 */
 
+
+//Result in form Number
 const roundedNumber = 3.6
+const decimal = roundedNumber - Math.floor(roundedNumber)
+
+let result = 0
+
+if (decimal > 0.5) {
+    result = Math.ceil(roundedNumber);
+} else {
+    result = Math.floor(roundedNumber);
+}
+console.log(result);
+
+// Result in form String
+
+const strNum = roundedNumber.toString()
+
+const decimalIndex = strNum.indexOf(".")
+const strDecimal = "0" + strNum.substring(decimalIndex)
+
+//let intDecimal = parseInt(strDecimal); // Not necessary
+
+let strResult = 0
+
+if (strDecimal > 0.5) {
+    strResult = Math.ceil(roundedNumber);
+} else {
+    strResult = Math.floor(roundedNumber);
+}
+console.log(strResult);
+
