eb55fb0eda3edecd7e36631f2cdd5b45f76dbe7c
Amir <amirhatam136@gmail.com>
2022-02-20 09:47:05 +0100

    jour-3, bonus-2

---
+++ jour-3/bonus-2.js
@@ -0,0 +1,13 @@
+// Reproduisez l'exercice 5 avec une boucle while
+
+let total = 0
+const limit = 10
+
+let i = 0
+while (i < limit) {
+    i++
+    console.log(i);
+    total += i
+}
+console.log(total);
+
