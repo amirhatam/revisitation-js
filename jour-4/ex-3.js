06d2f7a0c8385d3f7d8bb82293ec1b551fbae649
Amir <amirhatam136@gmail.com>
2022-02-20 20:50:54 +0100

    jour-4, ex-3

---
+++ jour-4/ex-3.js
@@ -0,0 +1,16 @@
+/*
+- Créez une fonction `checkIfEven` qui reçoit un paramètre `num`
+- Dans la fonction, à l'aide d'une condition, affichez "even" si `num` est pair, "odd" si il est impair
+- Appelez votre fonction plusieurs fois avec des chiffres différents comme argument pour vérifier les résultats
+*/
+
+function checkIfEven(num) {
+    if (num % 2 == 0) {
+        console.log("even");
+    } else {
+        console.log("odd");
+    }
+}
+
+
+checkIfEven(100)
