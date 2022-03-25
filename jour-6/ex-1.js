394d2f4e54b4e9b9ef75d6749a6b14a07ad9f4aa
Amir <amirhatam136@gmail.com>
2022-03-25 18:06:50 +0100

    Jour 6, ex-1, PROF Solution :|

--- jour-6/ex-1.js
+++ jour-6/ex-1.js
@@ -1,4 +1,20 @@
 //my Solution (BEST Practices)
-const formatDate = (d) => ["0" + new Date(d).getDate(), "0" + new Date(d).getMonth(), "" + new Date(d).getFullYear()]
+const formatDate = d => `0${new Date(d).getDate()}-0${new Date(d).getMonth()}-${new Date(d).getFullYear()}`
 
 console.log(formatDate("2022-03-02"));
+
+// PROF Solution :|
+function formatDateProf(dateString) {
+
+    var date = new Date(dateString);
+
+    // console.log("hi".padStart(5, "*"));
+
+    var jj = date.getDate().toString().padStart(2, "0");
+    var mm = (date.getMonth() + 1).toString().padStart(2, "0");
+
+    console.log("Result:", `${jj}/${mm}/${date.getFullYear()}`)
+
+}
+
+formatDateProf("2020-11-15")
