16f409a383f1d2c07dae7d62eae8f7004268a564
Amir <amirhatam136@gmail.com>
2022-03-26 09:51:51 +0100

    jour-6, bonus part two

--- jour-6/bonus.js
+++ jour-6/bonus.js
@@ -1,5 +1,9 @@
 const moment = require('moment')
 
-const formatDate = d => moment(d).format("DD-MM-YYYY")
 
+const formatDate = d => moment(d).format("DD-MM-YYYY")
 console.log(formatDate("2020-10-10"));
+
+
+const calculateAge = d => moment(d).fromNow()
+console.log(calculateAge("1986-10-10"));
