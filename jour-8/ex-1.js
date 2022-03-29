d7c5a1e91dd67cbc191b803cd86d88e66b7c517b
Amir <amirhatam136@gmail.com>
2022-03-29 10:29:52 +0200

    jour-8, ex-2

--- jour-8/ex-1.js
+++ jour-8/ex-1.js
@@ -1,12 +1,13 @@
 const request = require("request")
 
-// request.get("https://restcountries.com/v2/all", (err, res, body) => {
-//     const result = JSON.parse(body)
-//     let countries = result.map(e => {
-//         return e.name
-//     })
-//     console.log(countries.join(" - "));
-// })
+
+request.get("https://restcountries.com/v2/all", (err, res, body) => {
+    const result = JSON.parse(body)
+    let countries = result.map(e => {
+        return e.name
+    })
+    console.log(countries.join(" - "));
+})
 
 
 //Short way
---
+++ jour-8/ex-2.js
@@ -0,0 +1,7 @@
+const request = require("request")
+
+request.get("https://api.chucknorris.io/jokes/random", (err, res, body) => {
+    const result = JSON.parse(body)
+    console.log(result.value);
+})
+
