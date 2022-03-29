bc2b931353c537ae547aa9be04abdca2359b8fc2
Amir <amirhatam136@gmail.com>
2022-03-29 10:13:10 +0200

    jour-8, ex-1, short way

--- jour-8/ex-1.js
+++ jour-8/ex-1.js
@@ -1,9 +1,13 @@
 const request = require("request")
 
-request.get("https://restcountries.com/v2/all", (err, res, body) => {
-    const result = JSON.parse(body)
-    let countries = result.map(e => {
-        return e.name
-    })
-    console.log(countries.join(" - "));
-})
+// request.get("https://restcountries.com/v2/all", (err, res, body) => {
+//     const result = JSON.parse(body)
+//     let countries = result.map(e => {
+//         return e.name
+//     })
+//     console.log(countries.join(" - "));
+// })
+
+
+//Short way
+request.get("https://restcountries.com/v2/all", (err, res, body) => console.log(JSON.parse(body).map(e => e.name).join(" - ")))
