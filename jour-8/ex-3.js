219c7b5fcc83e79cdf146cdcf234eb263c4f1ef9
Amir <amirhatam136@gmail.com>
2022-03-29 11:00:25 +0200

    jour-8, ex-3, Another solution

--- jour-8/ex-3.js
+++ jour-8/ex-3.js
@@ -3,8 +3,20 @@ const request = require("request")
 //One solution
 const userInput = process.argv.slice(2)
 
-request.get("https://pokeapi.co/api/v2/pokemon/" + userInput, (err, res, body) => {
+request.get("https://pokeapi.co/api/v2/pokemon/" + userInput, function (err, res, body) {
     const result = JSON.parse(body)
-    console.log(result);
+    console.log("name : ", result.name);
 })
 
+
+//Another solution
+const findPokemon = (p) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${p}`
+
+    request.get(url, (err, res, body) => {
+        const result = JSON.parse(body)
+        console.log("name : ", result.name);
+    })
+}
+
+findPokemon(25)
